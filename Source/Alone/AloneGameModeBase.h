4709e4c622c7249a8b186accd975e62a00664d86
DESKTOP-3APC6A8\egagg <egiyazarov.s@gmail.com>
2022-05-24 14:06:07 +0300

    Create project Alone

---
+++ Source/Alone/AloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALONE_API AAloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
